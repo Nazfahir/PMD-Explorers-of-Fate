5532603f71347471b10f614811abc5295a5610e0
Nazfahir <nazfahir@gmail.com>
2025-09-18 12:47:16 -0300

    Rename system identifier to PMD-Explorers-of-Fate

--- module/actor-sheet.js
+++ module/actor-sheet.js
@@ -3,8 +3,8 @@ export class MyActorSheet extends ActorSheet {
   /** @override */
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ["my-basic-system", "sheet", "actor"],
-      template: "systems/my-basic-system/templates/actor-sheet.hbs",
+      classes: ["PMD-Explorers-of-Fate", "sheet", "actor"],
+      template: "systems/PMD-Explorers-of-Fate/templates/actor-sheet.hbs",
       width: 500,
       height: 600,
       tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "stats" }],
